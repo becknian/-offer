f28b02e00f81b05f8ff67d6e050bbae25a075fcd
becknian <32223253+becknian@users.noreply.github.com>
2018-03-08 18:02:13 -0500

    Create Solution.java

---
+++ 3 Print List From Tail To Head/Solution.java
@@ -0,0 +1,43 @@
+/*
+面试小提示：在面试中如果我们打算修改输入的数据，最好先问面试官是不是允许做修改
+*/
+
+// version 1: use stack (last-in first-out)
+import java.util.ArrayList;
+import java.util.Stack;
+public class Solution {
+    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+        Stack<Integer> stack = new Stack<>();
+        ArrayList<Integer> result = new ArrayList<>();
+        ListNode node = listNode;
+        while (node != null){
+            stack.push(node.val);
+            node = node.next;
+        }
+        while (!stack.isEmpty()){
+            result.add(stack.pop());
+        }
+        // stack.pop(): Removes the object at the top of this stack and returns that object as the value of this function.
+        // stack.peek(): Looks at the object at the top of this stack without removing it from the stack.
+        return result;
+    }
+}
+
+// version 2: inplace but will change the input ListNode
+public class Solution {
+    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+        ListNode prev = null;
+        while (listNode != null){
+            ListNode temp = listNode.next;
+            listNode.next = prev;
+            prev = listNode;
+            listNode = temp;
+        }
+        ArrayList<Integer> result = new ArrayList<>();
+        while (prev != null){
+            result.add(prev.val);
+            prev = prev.next;
+        }
+        return result;
+    }
+}
