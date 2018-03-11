f8f509bd68dcbcccaea47a5d92e5ce969168bbb2
becknian <32223253+becknian@users.noreply.github.com>
2018-03-10 22:39:53 -0500

    Create Solution.java

---
+++ 14 Reverse Linked List/Solution.java
@@ -0,0 +1,38 @@
+/*
+Reverse a linked list.
+
+Example
+For linked list 1->2->3, the reversed linked list is 3->2->1
+
+Challenge 
+Reverse it in-place and in one-pass
+*/
+/**
+ * Definition for ListNode
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param head: n
+     * @return: The new head of reversed linked list.
+     */
+    public ListNode reverse(ListNode head) {
+        // write your code here
+        ListNode prev = null;
+        while (head != null){
+            ListNode temp = head.next;
+            head.next = prev;
+            prev = head;
+            head = temp;
+        }
+        return prev;
+    }
+}
